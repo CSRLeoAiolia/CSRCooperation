5d8ac7a71fee8afdfaf7492ce2140be704ff872c
LeoAiolia <student@run.local>
2016-06-01 10:49:02 +0800

    LeoAiolia
    一些比较优秀的宏定义

---
+++ Header.h
@@ -0,0 +1,12 @@
+//
+//  Header.h
+// 优秀的宏定义 收集
+
+/**
+ *  此工程使用arc环境，如果不是，报警告
+ */
+#if ! __has_feature(objc_arc)
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
+
+
