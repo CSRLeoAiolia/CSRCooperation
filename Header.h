d886ba71fa043d95314c8b0e6cbd7a146b8ea3a6
LeoAiolia <student@run.local>
2016-06-01 11:13:53 +0800

    run

--- Header.h
+++ Header.h
@@ -3,10 +3,11 @@
 // 优秀的宏定义 收集
 
 /**
- *  此工程使用arc环境，如果不是，报警告
+ *  此工程使用arc环境，如果不是，报警告 --润哥写的
  */
 #if ! __has_feature(objc_arc)
 #warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
 #endif
-#define hahah
+
+//#define hahah
 
